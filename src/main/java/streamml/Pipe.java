aaea8b11a769129e0ef8aa5062554fabf357ed73
dobachi <dobachi1983oss@gmail.com>
2019-12-20 15:54:22 +0900

    Update process as a test

--- src/main/java/streamml/Pipe.java
+++ src/main/java/streamml/Pipe.java
@@ -18,6 +18,7 @@ package streamml;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
@@ -34,16 +35,20 @@ public class Pipe {
 
     public static void main(String[] args) throws Exception {
         Properties props = new Properties();
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-pipe");
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafka-streams-ml");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
         final StreamsBuilder builder = new StreamsBuilder();
 
-        builder.stream("streams-plaintext-input").to("streams-pipe-output");
+	KStream<String, String> source = builder.stream("test-input");
+	KStream<String, String> words = source.mapValues(value -> value + value);
+        words.to("test-output");
 
         final Topology topology = builder.build();
+	System.out.println(topology.describe());
+
         final KafkaStreams streams = new KafkaStreams(topology, props);
         final CountDownLatch latch = new CountDownLatch(1);
 
